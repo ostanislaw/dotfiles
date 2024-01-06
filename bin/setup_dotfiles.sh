e2b03539fbaf6ac89f96f2386b92098a09567fd9
Stanislaw Ostrowski <52103381+ostanislaw@users.noreply.github.com>
2024-01-06 21:12:10 +0100

    fixed setup script

--- bin/setup_dotfiles.sh
+++ bin/setup_dotfiles.sh
@@ -2,14 +2,13 @@ git clone --bare git@github.com:ostanislaw/dotfiles.git $HOME/.cfg
 function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
-mkdir -p .config-backup
+mkdir -p $HOME/.cfg-backup
 config checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    config checkout 2>&1 | egrep "^\s+\w*" | awk {'print $1'} | xargs -I{} sh -c 'mkdir -p $HOME/.cfg_backup/$(dirname {}) && mv {} $HOME/.cfg_backup/{}'
 fi;
 config checkout
 config config status.showUntrackedFiles no
-
